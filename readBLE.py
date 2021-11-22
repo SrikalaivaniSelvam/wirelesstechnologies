493fa47887863a2142403fec0b1fe3278096ccc6
srikalaivani <srikalaivani.panner@tcs.com>
2021-11-22 15:42:01 +0530

    first commit

---
+++ readBLE.py
@@ -0,0 +1,103 @@
+from bluepy import btle
+
+MAC = "c4:4f:33:08:32:bb"
+SERVICE_UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+print("Connect to:" + MAC)
+dev = btle.Peripheral(MAC)
+
+print("\n--- dev ----------------------------")
+print(type(dev))
+print(dev)
+
+print("\n--- dev.services -------------------")
+for svc in dev.services:
+    print(str(svc))
+    
+print("\n------------------------------------")
+print("Get Serice By UUID: " + SERVICE_UUID)
+service_uuid = btle.UUID(SERVICE_UUID)
+service = dev.getServiceByUUID(service_uuid)
+
+print(service)
+print("\n--- service.getCharacteristics() ---")
+print(type(service.getCharacteristics()))
+print(service.getCharacteristics())
+
+#----------------------------------------------
+characteristics = dev.getCharacteristics()
+print("\n--- dev.getCharacteristics() -------")
+print(type(characteristics))
+print(characteristics)
+
+for char in characteristics:
+    print("----------")
+    print(type(char))
+    print(char)
+    print(char.uuid)
+    if(char.uuid == CHARACTERISTIC_UUID ):
+        print("=== !CHARACTERISTIC_UUID matched! ==")
+        print(char)
+        print(dir(char))
+        print(char.getDescriptors)
+        print(char.propNames)
+        print(char.properties)
+        print(type(char.read()))
+        print(char.read())
+    
+#print("=== dev ============================")
+#print(dir(dev))
+#print("=== service ========================")
+#print(dir(service))from bluepy import btle
+
+MAC = "24:0a:c4:e8:0f:9a"
+SERVICE_UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+print("Connect to:" + MAC)
+dev = btle.Peripheral(MAC)
+
+print("\n--- dev ----------------------------")
+print(type(dev))
+print(dev)
+
+print("\n--- dev.services -------------------")
+for svc in dev.services:
+    print(str(svc))
+    
+print("\n------------------------------------")
+print("Get Serice By UUID: " + SERVICE_UUID)
+service_uuid = btle.UUID(SERVICE_UUID)
+service = dev.getServiceByUUID(service_uuid)
+
+print(service)
+print("\n--- service.getCharacteristics() ---")
+print(type(service.getCharacteristics()))
+print(service.getCharacteristics())
+
+#----------------------------------------------
+characteristics = dev.getCharacteristics()
+print("\n--- dev.getCharacteristics() -------")
+print(type(characteristics))
+print(characteristics)
+
+for char in characteristics:
+    print("----------")
+    print(type(char))
+    print(char)
+    print(char.uuid)
+    if(char.uuid == CHARACTERISTIC_UUID ):
+        print("=== !CHARACTERISTIC_UUID matched! ==")
+        print(char)
+        print(dir(char))
+        print(char.getDescriptors)
+        print(char.propNames)
+        print(char.properties)
+        print(type(char.read()))
+        print(char.read())
+    
+#print("=== dev ============================")
+#print(dir(dev))
+#print("=== service ========================")
+#print(dir(service))
